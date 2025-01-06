8b05176de481892bc7c2e4be6f7201c7cb0dbf80
Johan <johan.svensson@gtsmarketing.se>
2025-01-06 16:06:40 +0100

    price change

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import Footer from '$lib/components/Footer.svelte';
-
 	import { i18n } from '$lib/i18n';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	import Navbar from '$lib/components/Navbar.svelte';
@@ -9,7 +8,11 @@
 </script>
 
 <ParaglideJS {i18n}>
-	<Navbar />
-	{@render children()}
-	<Footer />
+	<div class="flex min-h-screen flex-col">
+		<Navbar />
+		<main class="flex-1">
+			{@render children()}
+		</main>
+		<Footer />
+	</div>
 </ParaglideJS>
